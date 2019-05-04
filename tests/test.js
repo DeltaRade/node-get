30bba47bb1d515cec7c7ef4d4202b4fd04996039
DeltaRade <coolxpress26@gmail.com>
2019-05-04 16:04:13 -0400

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,8 @@
 const get = require('../index');
 async function f() {
-	let data = await get('https://www.reddit.com/r/dankmemes/hot.json', {
-		method: 'GET',
-	});
-    data = data.json();
-    let memes=data.data.children
+	let data = await get('https://www.reddit.com/r/dankmemes/hot.json');
+	data = data.json();
+	let memes = data.data.children;
 	for (let i in memes) {
 		console.log(memes[i].data.url);
 	}
